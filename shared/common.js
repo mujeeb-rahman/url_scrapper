0dbffcdb2b6e68c75196c58eae7e58dd1a2374e9
Mujeeb Rahman <mujeebrahman296@gmail.com>
2022-04-04 02:05:16 +0530

    serverless-aws-documentation

--- shared/common.js
+++ shared/common.js
@@ -1,18 +1,26 @@
 const { httpConstants, errorConstants } = require('./constant');
 
+const headers = { 
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin' : '*'
+}
+
 const createResponse = (body, statusCode) => {
     return {
       statusCode,
-      body: JSON.stringify({ data: body })
+      body: JSON.stringify({ data: body }),
+      headers
     };
 };
   
 const createErrorResponse = (errorMessage, statusCode)=> {
     let code = statusCode ? statusCode : httpConstants.BAD_REQUEST;
     let message = errorMessage ? errorMessage : errorConstants.BAD_REQUEST;
+
     return {
         code,
-        body: JSON.stringify({ error: message })
+        body: JSON.stringify({ error: message }),
+        headers
     };
 };
 
